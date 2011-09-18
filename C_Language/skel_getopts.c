3a2a7fe7aa076698446ce57ab43cf20b8ffc2c7f
Philippe Pittoli <kane.root@gmail.com>
2011-09-19 00:00:39 +0200

    works

--- C_Language/skel_getopts.c
+++ C_Language/skel_getopts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[])
 {
@@ -28,6 +29,7 @@ int main(int argc, char * argv[])
 				fprintf(stdout,"erreur : %s\n",optopt);
 				break;
 		}
+	}
 	// s'il reste des options
 	if(argc != optind)
 	{
