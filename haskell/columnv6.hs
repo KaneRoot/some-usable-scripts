e71053e3b880cd930ed48b0769857cdb8875d443
karchnu <karchnu@karchnu.fr>
2016-01-21 18:33:31 +0100

    gratté 2 caractères et une ligne vide, toujours ça de pris

--- haskell/columnv6.hs
+++ haskell/columnv6.hs
@@ -1,5 +1,4 @@
 import Data.List as L
-
 main = do content <- getContents
-          putStr $ unlines $ map unwords $ L.transpose $ map (completeMatrix ' ') $ L.transpose $ (completeMatrix "") $ map words $ (lines content)
+          putStr $ unlines $ map unwords $ L.transpose $ map (completeMatrix ' ') $ L.transpose $ completeMatrix "" $ map words $ lines $ content
           where completeMatrix v m = map (\e -> e ++ [v| _ <- [length e .. (\ x -> maximum $ map length x) m ]]) m
