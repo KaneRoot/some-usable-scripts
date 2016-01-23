147b15593038574dd502739a1c9d1ad607259e3f
Karchnu <karchnu@karchnu.fr>
2016-01-23 13:13:49 +0100

    219 octets, difficile à optimiser là \o/

--- haskell/columnv7.hs
+++ haskell/columnv7.hs
@@ -1,3 +1,3 @@
 import Data.List
-main = do interact $ (unlines . map unwords . transpose . map (pad ' ') . transpose . pad "" . map words . lines)
-          where pad v m = map (\e -> e ++ replicate ((maximum $ map length m)- length e) v) m
+main = interact $ (unlines . map unwords . transpose . map (pad ' ') . transpose . pad "" . map words . lines)
+       where pad v m = map (\e -> e ++ replicate ((maximum $ map length m)- length e) v) m
