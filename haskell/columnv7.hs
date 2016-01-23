df467a8133aac0482f5721edbe77f40be74b821d
Karchnu <karchnu@karchnu.fr>
2016-01-23 01:36:46 +0100

    completeMatrix => pad

--- haskell/columnv7.hs
+++ haskell/columnv7.hs
@@ -1,3 +1,3 @@
 import Data.List
-main = do interact $ (unlines . map unwords . transpose . map (completeMatrix ' ') . transpose . completeMatrix "" . map words . lines)
-          where completeMatrix v m = map (\e -> e ++ replicate ((maximum $ map length m)- length e) v) m
+main = do interact $ (unlines . map unwords . transpose . map (pad ' ') . transpose . pad "" . map words . lines)
+          where pad v m = map (\e -> e ++ replicate ((maximum $ map length m)- length e) v) m
