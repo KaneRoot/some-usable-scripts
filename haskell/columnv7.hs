63a602b979979042cc40387cf8aab20c11a8b167
Karchnu <karchnu@karchnu.fr>
2016-01-23 14:11:47 +0100

    $ inutile avec interact

--- haskell/columnv7.hs
+++ haskell/columnv7.hs
@@ -1,3 +1,3 @@
 import Data.List
-main = interact $ (unlines . map unwords . transpose . map (pad ' ') . transpose . pad "" . map words . lines)
+main = interact (unlines . map unwords . transpose . map (pad ' ') . transpose . pad "" . map words . lines)
        where pad v m = map (\e -> e ++ replicate ((maximum $ map length m)- length e) v) m
